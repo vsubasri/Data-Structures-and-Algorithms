70f790e8d4e1afc9b87a1fac4e46915e2f96113e
vsubasri <vsubasri@uwo.ca>
2017-05-07 20:01:12 -0400

    Create TestDoublyLinkedList.java

---
+++ DoublyLinkedList/TestDoublyLinkedList.java
@@ -0,0 +1,39 @@
+ /**
+    *             Assignment1, COMP272
+    * Class:      TestDoublyLinkedList.java
+    * Purpose:    Tests the DoublyLinkedList class.
+    *
+    * @author:    Valli Subasri
+    * Student ID: 3286510
+    * Date:       August 28, 2016
+    * Version     1.0
+    *
+    */
+
+public class TestDoublyLinkedList {
+	
+	public static void main (String[] args) {
+		DoublyLinkedList test = new DoublyLinkedList();
+		test.addLast("a2");
+		test.addLast("a1");
+		test.addLast("a3");
+		test.addLast("a4");
+		test.addLast("a6");
+		test.addLast("a5");
+		test.addLast("a7");
+		test.addLast("a8");
+		test.addLast("a10");
+		test.addLast("a9");
+		test.printList();
+		test.swap("a2", "a1");		//test run 1
+		test.printList();
+		test.swap("a5", "a6");		//test run 2
+		test.printList();
+		test.swap("a9", "a10");		//test run 3
+		test.printList();
+		test.swap("a1", "a10");		//test run 4
+		test.printList();
+		test.swap(1, 2);			//test run 5
+		test.printList();
+	}
+}
